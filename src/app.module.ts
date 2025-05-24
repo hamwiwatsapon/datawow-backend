07ca6f9f7636d5373728441b553ce0679a30b4f5
wiwatsapon <wiwatsapon.l@kanda.digital>
2025-05-24 11:32:38 +0700

    :wrench:(App):Update module config to use db sqlitie and read entity from all file entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
     UsersModule,
     PostsModule,
     CommentsModule,
+    AuthModule,
   ],
+  providers: [AuthService],
 })
 export class AppModule { }
