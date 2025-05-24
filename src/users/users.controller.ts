85bec44c184e0de43e116012e4fd9a0a2a9198c1
wiwatsapon <wiwatsapon.l@kanda.digital>
2025-05-24 11:34:42 +0700

    :wrench:(User):Implement Service, Modul, Controller of user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) { }
+
+  @Post('login')
+  async login(@Body('username') username: string) {
+    let user = await this.usersService.findByUsername(username);
+    user ??= await this.usersService.create(username);
+    return user;
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private readonly repo: Repository<User>) { }
+
+  create(username: string) {
+    const user = this.repo.create({ username });
+    return this.repo.save(user);
+  }
+
+  findByUsername(username: string) {
+    return this.repo.findOne({ where: { username } });
+  }
+
+  findOne(id: number) {
+    return this.repo.findOne({ where: { id } });
+  }
+}
